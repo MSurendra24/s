47f91a8a337005512156202628470029ec40baf1
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-01 10:45:11 +0530

    first

---
+++ customer.js
@@ -0,0 +1,3 @@
+var mobile_num=123456789n;
+console.log(mobile_num);
+console.log(typeof mobile_num);
---
+++ product.js
@@ -0,0 +1,8 @@
+var price=399;
+var rating=4.1;
+var prod_name="casual shirt";
+var available='y';
+console.log(price);
+console.log(typeof price);
+
+
