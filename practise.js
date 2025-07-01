2189e49126b90615e622d9791107227d3d099636
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-01 20:03:19 +0530

    tasks12

---
+++ practise.js
@@ -0,0 +1,28 @@
+const a=95
+console.log(a);
+console.log(typeof a);
+var c;
+console.log(c);
+console.log(typeof c);
+var c=93
+console.log(c);
+console.log(typeof c);
+let b=["surendra","practise", "javascript"];
+console.log(b);
+console.log(typeof b);
+let s=[1,2,3,4,5];
+console.log(s);
+console.log(typeof s);
+console.log(Array.isArray(s));
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits);
+console.log(typeof fruits);
+console.log(Array.isArray(fruits));
+let person = {
+name: "sru",
+age: 24,
+isStudent: false
+};
+console.log(person);
+console.log(typeof person);
+console.log(Array.isArray(person));
